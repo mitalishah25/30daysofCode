5c6e64bc90da283b643ce1b31401c8902f9ba2ad
mitalishah25 <32921585+mitalishah25@users.noreply.github.com>
2018-11-30 19:55:21 -0800

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,31 @@
+# Problem Statement: https://www.hackerrank.com/challenges/30-recursion/problem
+#!/bin/python3
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+
+    if n == 0:
+        return n
+    elif n == 1:
+        return n
+    else:     
+       return n*factorial(n-1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
